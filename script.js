4a4a99aa2880fa5f6e1907d9914f9f2ebe39ebbd
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-01-25 19:02:08 +0300

    This will add the finished language circular progress bars

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const numb = document.querySelector(".numb");
-let counter = 0;
-setInterval(()=>{
-    if (counter == 100){
-        clearInterval();
-    }else{
-        counter+=1;
-        numb.textContent = counter + "%"
-    }
-}, 80);
+let options = {
+    startAngle:-1.55,
+    size: 50,
+    value: 0.9,
+    fill: {gradient: ['#a445b2', '#fa4299']}
+}
+$(".circle .bar").circleProgress(options).on('circle-animation-progress',
+function(event, progress, stepValue){
+});
+$(".swahili .bar").circleProgress({
+    value: 1.0,
+});
+$(".french .bar").circleProgress({
+    value: 0.2,
+});
+$(".chinese .bar").circleProgress({
+    value: 0.03,
+});
