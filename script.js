e026fbd2f17c4dc4a910c200958570ec11292835
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-02-04 10:15:59 +0300

    This will ensure only Purple, white and Gold will be used in the website

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let options = {
     startAngle:-1.55,
     size: 50,
     value: 0.9,
-    fill: {gradient: ['#a445b2', '#fa4299']}
+    fill: {gradient: ['gold']}
 }
 $(".circle .bar").circleProgress(options).on('circle-animation-progress',
 function(event, progress, stepValue){
